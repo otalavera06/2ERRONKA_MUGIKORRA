3c0dce51abc0a4c2cf5f8dcb7b4c9120f7130ccb
otalavera06 <oie_tal_mun@goierrieskola.org>
2026-05-07 16:01:32 +0200

    Zerbitzarian konektatu ahal izateko

--- app/src/main/java/com/example/taldea5/NetworkConfig.kt
+++ app/src/main/java/com/example/taldea5/NetworkConfig.kt
@@ -1,12 +1,10 @@
 package com.example.taldea5
 
 object NetworkConfig {
-    // Para el emulador, usa "10.0.2.2"
-    // Para tablet fisica por Wi-Fi o red local, pon la IP de tu ordenador (ej. "192.168.1.XX" o "172.16.242.198")
-    private const val PC_HOST = "172.16.243.23"
+    private const val PC_HOST = "192.168.10.5"
 
     val apiBaseUrl: String
-        get() = "http://$PC_HOST:5005/"
+        get() = "http://$PC_HOST:5093/"
 
     val chatHost: String
         get() = PC_HOST
